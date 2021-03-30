da91e93de6721f09ef71fb3599cb413e13e2fc0c
Florian Brunner <florianbrunner26042001@gmail.com>
2021-03-30 21:13:28 +0200

    Adding ViewModel with basic functionality

---
+++ TourPlanner/ViewModels/TourPlannerVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+using System.ComponentModel;
+namespace TourPlanner.ViewModels
+{
+    class TourPlannerVM:ViewModelBase
+    {
+        public ObservableCollection<Tour> Tours { get; private set; }
+        public TourPlannerVM()
+        {
+            this.Tours = new ObservableCollection<Tour>();
+            this.AddCommand = new RelayCommand(() => Tours.Add(new Tour()));
+            this:RemoveCommand = new RelayCommand(() => Tours.RemoveAt(Tours.Count));
+        }
+    }
+}
