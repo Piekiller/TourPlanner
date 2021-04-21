cb625de1cad7e1c1be219a047e88d0fcfab07eb6
Florian Brunner <florianbrunner26042001@gmail.com>
2021-04-21 16:45:07 +0200

    Implementing Async await in DAL

--- TourPlanner.DataAccessLayer/IDataAccess.cs
+++ TourPlanner.DataAccessLayer/IDataAccess.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using TourPlanner.Models;
 namespace TourPlanner.DataAccessLayer
 {
     public interface IDataAccess
     {
-        public void SaveTour(Tour t);
-        public List<Tour> GetTours();
-        public void DeleteTour(Tour t);
-        public void UpdateTour(Tour t);
+        public event EventHandler<Tour> AddedTour;
+        public event EventHandler<TourLog> AddedTourLog;
 
-        public void SaveTourLog(TourLog tl);
-        public List<TourLog> GetTourLogs(Tour t);
-        public void DeleteTourLog(TourLog tl); 
-        public void UpdateTourLog(TourLog tl);
+        public Task SaveTour(Tour t);
+        public Task<List<Tour>> GetTours();
+        public Task DeleteTour(Tour t);
+        public Task UpdateTour(Tour t);
+
+        public Task SaveTourLog(TourLog tl);
+        public Task<List<TourLog>> GetTourLogs(Tour t);
+        public Task DeleteTourLog(TourLog tl); 
+        public Task UpdateTourLog(TourLog tl);
 
 
     }
