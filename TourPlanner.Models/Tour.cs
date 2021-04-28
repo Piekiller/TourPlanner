869620c9b5561ffe9118f03edfc8c8d84027fe22
Florian Brunner <florianbrunner26042001@gmail.com>
2021-04-28 20:52:54 +0200

    Add startpoint and endpoint to Tour

--- TourPlanner.Models/Tour.cs
+++ TourPlanner.Models/Tour.cs
@@ -9,24 +9,30 @@ namespace TourPlanner.Models
         public string Description { get; private set; }
         public string RouteInformation { get; private set; }
         public double Distance { get; private set; }
+        public string StartPoint { get; private set; }
+        public string EndPoint { get; private set; }
         public Guid Id { get; private set; }
 
-        public Tour(string name, string description, string routeInformation, double distance)
+        public Tour(string name, string description, string routeInformation, double distance, string startpoint,string endpoint)
         {
             Name = name;
             Description = description;
             RouteInformation = routeInformation;
             Distance = distance;
             Id = Guid.NewGuid();
+            StartPoint = startpoint;
+            EndPoint = endpoint;
         }
 
-        public Tour(string name, string description, string route_Information, double distance, Guid id) : this(name, description, route_Information, distance)
+        public Tour(string name, string description, string route_Information, double distance, string startpoint, string endpoint, Guid id) : this(name, description, route_Information, distance, startpoint, endpoint)
         {
             Id = id;
         }
 
         public bool Equals([AllowNull] Tour other)
         {
+            if (other is null)
+                return false;
             return Id.Equals(other.Id);
         }
     }
