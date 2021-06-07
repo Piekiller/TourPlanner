1a4904dd1e26933b50e9acb433d4b1275124d854
Florian Brunner <florianbrunner26042001@gmail.com>
2021-06-07 18:15:35 +0200

    Fix Searchbar and Import now loads Tours and gives them a new Id and saves them

--- TourPlanner/ViewModels/TourPlannerVM.cs
+++ TourPlanner/ViewModels/TourPlannerVM.cs
@@ -22,13 +22,11 @@ namespace TourPlanner.ViewModels
     public class TourPlannerVM : ViewModelBase
     {
         public ObservableCollection<Tour> Tours { get; set; } = new();
-        public ObservableCollection<Tour> SearchedTours { get; set; } = new();
         private Tour _selectedTour;
         private TourLog _selectedTourLog;
 
         public Tour SelectedTour { get => _selectedTour; set { _selectedTour = value; base.RaisePropertyChangedEvent(); } }
         public TourLog SelectedTourLog { get => _selectedTourLog; set { _selectedTourLog = value; base.RaisePropertyChangedEvent(); } }
-        public SearchTourVM TourVm { get; set; } 
 
         public RelayCommand AddTourWindowCommand { get; private set; }
         public RelayCommand AddTourLogWindowCommand { get; private set; }
@@ -41,7 +39,10 @@ namespace TourPlanner.ViewModels
         public RelayCommand UpdateTourLogCommand { get; private set; }
         public AsyncCommand ImportCommand { get; private set; }
         public AsyncCommand ExportCommand { get; private set; }
+        public AsyncCommand SearchCommand { get; private set; }
 
+        public string SearchTerm { get; set; } = "Search";
+        public bool CaseSensitve { get; set; } = false;
         private IMediator _mediator;
         private AddTour _addTour;
         private AddTourLog _addTourLog;
@@ -57,6 +58,7 @@ namespace TourPlanner.ViewModels
             this.UpdateTourLogCommand = new(OpenUpdateTourLogWindow);
             this.ImportCommand = new(Import);
             this.ExportCommand = new(Export);
+            this.SearchCommand = new(Search);
 
             Task.Factory.StartNew(async () =>
             {
@@ -67,9 +69,6 @@ namespace TourPlanner.ViewModels
                     {
                         Tours.Add(t);
                     }); //Necessary because ObservableCollection needs to be used by the UI Thread.
-                    foreach (var item in Tours)
-                        SearchedTours.Add(item);
-                    TourVm = new SearchTourVM(SearchedTours.ToList());
                 }));
             });
 
@@ -153,8 +152,13 @@ namespace TourPlanner.ViewModels
             {
                 data = await File.ReadAllTextAsync(openFileDialog.FileName);
                 List<Tour> tours = new(SerializationFactory.GetInstance().Deserialize(data));
-                SearchedTours.Clear();
-                tours.ForEach(tour => SearchedTours.Add(tour));
+                
+                tours.ForEach(async tour =>
+                {
+                    tour.Id = Guid.NewGuid();
+                    await TourFactory.GetInstance().CreateItem(tour);
+                    Tours.Add(tour);
+                });
             }
         }
         private async Task Export()
@@ -170,6 +174,12 @@ namespace TourPlanner.ViewModels
                 await sw.WriteLineAsync(data);
                 sw.Flush();
             }
+        }
+        private async Task Search()
+        {
+            List<Tour> tours = (await TourFactory.GetInstance().Search(SearchTerm, CaseSensitve)).ToList();
+            Tours.Clear();
+            tours.ForEach(tour => Tours.Add(tour));
         }
         public void SaveNewTour(Tour t)
         {
